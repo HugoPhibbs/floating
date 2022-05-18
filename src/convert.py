36c384e11fccd8bc412d08a5fcf32f46c9f88f58
Ben Forde <forbe404.otago.co.nz>
2022-05-18 14:25:48 +1200

    combined both pieces of work together into the one file. added documentation to mine.

--- src/convert.py
+++ src/convert.py
@@ -61,4 +61,64 @@ class Convert:
         else:
             return "{:064b}".format(bits)
 
+
+
+
+    def ibm_to_float(binary):
+        """
+        converts the bitpattern given into a float from ibm
+        :return: the float version of the binary ibm
+        """
+        sign_bit = binary[0:1]
+        sign = 0
+        if sign_bit == '1':
+            sign = -1
+        else:
+            sign = 1
+
+        # convert exponant binary to decimal value
+        exponant_binary = binary[1:8]
+        exponant = binary_to_decimal(exponant_binary)
+
+        # convert binary fraction
+        fraction_binary = binary[8:len(binary)]
+        hexi = hex(int(fraction_binary, 2))
+        find_index = hexi.index("x")
+        start = hexi[0:find_index]  # this is what changed if broken
+        middle = "0."
+        end = hexi[2:len(hexi)]
+        fraction_hex = start + middle + end
+        fraction = float.fromhex(fraction_hex)
+        ibm_to_float_math_single(sign, exponant, fraction)
+
+    def ibm_to_float_math_single(sign, exponant, fraction):
+        """
+        takes the decimal exponant and hexidecimal fraction and converts
+        to the float.
+        :param exponant: deciaml value of the binary transformed in previous method.
+        :param fraction: hexidecimal value of the binary transformed in previous method.
+        :return: float
+        """
+        convert = pow(sign, 1) * fraction * pow(16, exponant - 64);
+        print(convert)
+
+    def binary_to_decimal(binary):
+        """
+        converts binary to decimal
+        :return: decimal
+        """
+        binary = ''.join(reversed(binary))
+        decimal = 0
+        for i in range(0, len(binary)):
+            decimal += int(binary[i]) * 2 ** i
+        return decimal
+
+    if __name__ == '__main__':
+        ibm_to_float("11000010011101101010000000000000")
+        ibm_to_float("01111111111111111111111111111111")
+        ibm_to_float("00000000000100000000000000000000")
+        # dec = float.fromhex("0x0.76A000")
+        # binary_to_decimal("1000010")
+
+
 Convert().get_input()
